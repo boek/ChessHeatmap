f7e61baeb609a141edcffb676fb85ebf0827c2c3
Jeff Boek <jeff@jeffboek.com>
2023-04-06 15:31:24 -0700

    Initial Commit

---
+++ ChessHeatmap/ChessHeatmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessHeatmapApp.swift
+//  ChessHeatmap
+//
+//  Created by Jeff Boek on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ChessHeatmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
