c8ad72b1edcaf7e22a2d5b828b74f76e0d3009b6
maxdemaio <maxdman321@gmail.com>
2023-04-21 21:07:20 -0400

    wip add form start

--- ChessHeatmap/modules/CompareStats/CompareStatsView.swift
+++ ChessHeatmap/modules/CompareStats/CompareStatsView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct CompareStatsView: View {
+    @Environment(\.chessClient) var chessClient
+    @State var username1: String = ""
+    @State var username2: String = ""
+    @State var searching = false
+    @State private var availableYears: [Int] = []
+    @State private var year: Int = Calendar.current.component(.year, from: Date())
+    
+    private func setupAvailableYears() {
+        let currentYear = Calendar.current.component(.year, from: Date())
+        availableYears = Array(2007...currentYear)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack{
+                VStack (alignment: .leading) {
+                    Text("Username1")
+                    TextField("username1", text: $username1)
+                        .padding(10)
+                        .textInputAutocapitalization(.never)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                }
+                VStack (alignment: .leading) {
+                    Text("Username2")
+                    TextField("username2", text: $username2)
+                        .padding(10)
+                        .textInputAutocapitalization(.never)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 6)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                }
+            }.padding()
+            
+            HStack {
+                Picker("Year", selection: $year) {
+                    ForEach(availableYears, id: \.self) { year in
+                        Text(String(year))
+                            .tag(year)
+                    }
+                }
+                Button("Compare") {
+                    searching = true
+                }.buttonStyle(.bordered)
+                
+            }
+            .onAppear(perform: setupAvailableYears)
+            .padding(.vertical, 10)
+        }
+        
+        
     }
 }
 
