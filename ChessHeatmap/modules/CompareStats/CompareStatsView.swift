08021839723a39c967512623ae1ff6825b319641
maxdemaio <maxdman321@gmail.com>
2023-04-21 20:46:31 -0400

    wip create compare stats view

---
+++ ChessHeatmap/modules/CompareStats/CompareStatsView.swift
@@ -0,0 +1,20 @@
+//
+//  CompareStatsView.swift
+//  ChessHeatmap
+//
+//  Created by Maxwell DeMaio on 4/21/23.
+//
+
+import SwiftUI
+
+struct CompareStatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompareStatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompareStatsView()
+    }
+}
